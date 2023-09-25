aa867b9b99cc1713bcef7b06d55cb95d247797bb
CherpakovaAnastasiia <136085972+CherpakovaAnastasiia@users.noreply.github.com>
2023-09-25 18:42:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 // Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
 const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 let score = 0
 
-// Déclaration de la variable contenant le mot saisi par l'utilisateur
-// Le prompt sert à afficher une popup demandant à l'utilisateur de saisir un mot
-let motUtilisateur = prompt('Entrez le mot : ' + listeMots[0])
-
-// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-if (motUtilisateur === listeMots[0]) {
-    score++
+let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")}
+if (choix === "mots") {
+for (let i = 0, i < listeMots.lenght, i++) {
+    let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            score++}}
+    console.log("Votre score est de " + score + " sur " + listeMots.length)
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
+        if (phraseUtilisateur === listePhrases[i]) {
+            score++}}
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
 }
 
-// Idem avec le second mot du tableau
-// Notez l'absence du "let" devant la déclaration de la variable "motUtilisateur", cela permet de réutiliser la variable déclarée précédemment
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[1])
-if (motUtilisateur === listeMots[1]) {
-    score++
-}
 
-// Idem avec le troisième mot du tableau
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[2])
-if (motUtilisateur === listeMots[2]) {
-    score++
-}
-
-// Affichage du score de l'utilisateur
-console.log("Votre score est de " + score + " sur 3")
